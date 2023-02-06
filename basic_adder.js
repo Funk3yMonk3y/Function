f7d6b81b7fe48d8637a667c61cd8818124b17aef
Funk3yMonk3y <benjirueling@gmail.com>
2023-02-06 15:02:24 +0100

    first commit

---
+++ basic_adder.js
@@ -0,0 +1,13 @@
+//Function that adds parameters to calculate a sum.
+function add(a,b,c){
+return a+b+c;
+}
+
+//Initializing components
+let a = "this adds "
+let b = "three strings "
+let c = "to form a sentence"
+
+//Print the sum
+console.log(add(1,2,3))
+console.log(add(a,b,c))
